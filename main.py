be3f2136182059033ca85a6a22a5672a21941bac
DestaDag <dagimay1@gmail.com>
2022-05-10 17:55:09 +0200

    bug fix

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from pydoc import plain
 from turtle import right
 import pygame
 from pygame.locals import *
@@ -26,8 +27,8 @@ class Ball:
         self.centerX = random.randint(0, 600)
         self.centerY = random.randint(0, 600)
         self.color = Colors["red"]
-        self.velocityX = random.randint(-10, 10)
-        self.velocityY = random.randint(-10, 10)
+        self.velocityX = 2
+        self.velocityY = 2
         self.radius = 10
 
     def draw(self, screen):
@@ -35,38 +36,43 @@ class Ball:
 
     def update(self, screen):
 
-        #Detect collision
-        v1 = pygame.math.Vector2(self.centerX, self.centerY)
-        v2 = pygame.math.Vector2(Ball().centerX, Ball().centerY)
-        
-        hit = 1
-        if v1.distance_to(v2) <= self.radius + Ball().radius - 2: 
-            hit += 1
-            print("hit:", hit)
-
-            nv = v2 - v1
-            m1 = pygame.math.Vector2(self.velocityX, self.velocityY).reflect(nv)
-            m2 = pygame.math.Vector2(Ball().velocityX, Ball().velocityY).reflect(nv)
-
-            self.centerX = round(m1.x)
-            self.centerY = round(m1.y)
-
-            Ball().centerX = round(m2.x)
-            Ball().centerY = round(m2.y)
-
-
         if self.centerX <= 20 or self.centerY <= 20:
-            self.velocityX = random.randint(1, 10)
-            self.velocityY = random.randint(1, 10)
+            self.velocityX = random.randint(1,5)
+            self.velocityY = random.randint(1,5)
         if self.centerX >= 580 or self.centerY >= 580:
-            self.velocityX -= random.randint(1, 10)
-            self.velocityY -= random.randint(1, 10)
+            self.velocityX -= random.randint(1,5)
+            self.velocityY -= random.randint(1,5)
 
         self.centerX += self.velocityX
         self.centerY += self.velocityY
 
 
+class Player(Ball):
+    def __init__(self, centerX=None, centerY=None, direction=None, color=None, radius=None):
+        super().__init__(self,radius)
+        self.centerX = round(width/2)
+        self.centerY = round(height/2)
+        self.color = Colors["white"]
+    
+    def update(self, screen, ball = Ball()):
+        v1 = pygame.math.Vector2(self.centerX, self.centerY)
+        v2 = pygame.math.Vector2(ball.centerX, ball.centerY)
+        
+        hit = 1
+        if v1.distance_to(v2) <= self.radius + ball.radius - 2: 
 
+            nv = v2 - v1
+            m1 = pygame.math.Vector2(self.velocityX, self.velocityY).reflect(nv)
+            m2 = pygame.math.Vector2(ball.velocityX, ball.velocityY).reflect(nv)
+
+            self.centerX += round(m1.x)
+            self.centerY += round(m1.y)
+
+            ball.centerX += round(m2.x)
+            ball.centerY += round(m2.y)
+
+            print("Hit")
+        
 
 def main():
     screen = pygame.display.set_mode((width, height))
@@ -76,6 +82,8 @@ def main():
     clock = pygame.time.Clock()
 
     balls = [Ball() for _ in range(2)]
+    playa = Player()
+    
     timer = 1
     running = True
     while running:
@@ -87,8 +95,11 @@ def main():
         
         screen.fill(Colors["black"])
 
+        playa.draw(screen=screen)
+
         for ball in balls:
             ball.draw(screen =screen)
+            playa.update(screen=screen, ball=ball)
             ball.update(screen= screen)
 
         clock.tick(60)
